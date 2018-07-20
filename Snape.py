400d25ab10f7dd9343f92a400a7cb3deb10f09f8
Kai Xia <xiaket@gmail.com>
2018-07-20 14:02:49 +1000

    fix bugs in initial version:
    1. handle the case where db file is empty.
    2. properly save all snippet instances as json.
    3. Unify naming convention.
    4. other smaller things.
    
    Signed-off-by: Kai Xia <xiaket@gmail.com>

--- Snape.py
+++ Snape.py
@@ -32,9 +32,13 @@ class SnippetFile:
             return
 
         with open(self.DBFILE) as fobj:
+            content = fobj.read()
+            if not content.strip():
+                content = "{}"
+
             self.snippets = {
                 name: Snippet(name, data)
-                for name, data in json.loads(fobj.read()).items()
+                for name, data in json.loads(content).items()
             }
 
     def save(self):
@@ -42,6 +46,7 @@ class SnippetFile:
             fobj.write(
                 json.dumps(
                     self.snippets,
+                    default=lambda s: s.__json__,
                     indent=2,
                     sort_keys=True,
                     separators=(",", ": "),
@@ -61,21 +66,23 @@ class SnippetFile:
             except EOFError:
                 break
             except KeyboardInterrupt:
-                print("\nUser cancelled, not saving.")
+                print("\nUser cancelled, not saving.", file=sys.stderr)
                 sys.exit(1)
 
             contents.append(line)
 
         text = "\n".join(contents)
         lang = guess_lexer(text).name if HAS_PYGMENTS else ""
-        self.snippets[name] = {"text": text, "lang": lang}
+        self.snippets[name] = Snippet(
+            name, {"content": text, "lang": lang}
+        )
         self.save()
 
     def search(self, query):
         query = query.lower()
         for key, snippet in self.snippets.items():
-            if query in key.lower() or name in snippet.content:
-                output_pair(key, db[key])
+            if query in key.lower() or query in snippet.content.lower():
+                snippet.show()
 
     def __contains__(self, name):
         return name in self.snippets
@@ -84,8 +91,12 @@ class SnippetFile:
 class Snippet:
     def __init__(self, name, data):
         self.name = name
-        self.content = self.data["text"]
+        self.content = data["content"]
         self.lang = data["lang"]
+
+    @property
+    def __json__(self):
+        return {'content': self.content, 'lang': self.lang}
 
     @property
     def text(self):
@@ -97,14 +108,14 @@ class Snippet:
         else:
             lexer = guess_lexer(self.content)
 
-        return highlight(self.content, lexer, TerminalFormatter())
+        return highlight(self.content, lexer(), TerminalFormatter()).strip()
 
     def show(self):
-        print(f"\x1b[1;34m## {self.name} ##\x1b[0m")
+        print(f"\x1b[1;34m## {self.name}\x1b[0m\n")
         print("```")
         print(self.text)
         print("```")
-        print()
+        print("\n")
 
 
 def main():
@@ -119,7 +130,7 @@ def main():
         elif first == "add" and len(sys.argv) > 2:
             snippet_file.add(" ".join(sys.argv[2:]))
         elif first == "search" and len(sys.argv) > 2:
-            snippet_file.search(db, " ".join(sys.argv[2:]))
+            snippet_file.search(" ".join(sys.argv[2:]))
         else:
             print(USAGE)
             sys.exit(1)
